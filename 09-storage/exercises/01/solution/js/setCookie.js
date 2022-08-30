7d30feaf29ffaaba43ede49bc1131c6ec958f461
Istvan888 <marai.istvanp@gmail.com>
2022-08-30 14:00:37 +0200

    half done

--- 09-storage/exercises/01/solution/js/setCookie.js
+++ 09-storage/exercises/01/solution/js/setCookie.js
@@ -1,1 +1,16 @@
+// 1. Írj egy `setCookie` nevű függvényt, ami a paraméterként kapott stringet eltárolja
+// egy `token` nevű sütiben, ami 15 perc után lejár!
+'use strict';
+
+const setCookie = {
+    getCookie(name) {
+        const token = document.cookie
+        .split('; ')
+        .find(cookie => cookie.startsWith(`${name}=`))
+        
+    }
+}
+
+
+
 export default setCookie;
